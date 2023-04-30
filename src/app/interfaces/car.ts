fb6b15d19c7458dab927007661c931945ebfa542
lkuessner <leonkuessner@outlook.com>
2023-04-30 04:02:07 +0200

    encapsulate enums and refactor Car interface

--- src/app/interfaces/car.ts
+++ src/app/interfaces/car.ts
@@ -1,16 +1,19 @@
-import { Preis } from './preis';
+import {
+   CAR_BRANDS,
+   CAR_CONSTRUCTIONYEAR,
+   CAR_FUEL,
+   CAR_MODEL,
+   CAR_MODS,
+   CAR_SERIES,
+   CAR_SITS, CAR_TRANSMISSION,
+   CAR_VARIANTS
+} from "../mock/enums";
+
+type CarDetails = { model: CAR_MODEL, price: number, horsePower: number, seatsAmount: CAR_SITS, transmissionType: CAR_TRANSMISSION, fuelType: CAR_FUEL, brand: CAR_BRANDS, series: CAR_SERIES, variant: CAR_VARIANTS, modifications: CAR_MODS, yearOfConstruction: CAR_CONSTRUCTIONYEAR, fuelConsumptionRateInLiter: number, placeOfRental?: string }
 
 export interface Car {
    id: number,
-   knz: string,
-   bezeichnung: string,
-   model: string,
-   datum: number,
-   tank: string,
-   verbrauch: number,
-   getriebe: string,
-   ps: number,
-   sitze: number,
-   ort: string,
+   details: CarDetails
+   knz?: string,
    image?: string
 }
